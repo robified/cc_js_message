2aeb38b58ef19d5670acdb8ac2bca703c4c7c782
Robin <daorob@gmail.com>
2020-04-01 23:36:38 -0500

    Decode the message and the URL.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const { hash } = window.location;
+
+console.log(atob(hash.replace('#', '')));
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
     document.querySelector('#message-form').classList.add('hide');
-    document.querySelector('#link-form').classList.remove('hide');
 
+    document.querySelector('#link-form').classList.remove('hide');
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
