2b2e1ab1f6ec85c031957c7d42b082b2728de6f3
Robin <daorob@gmail.com>
2020-04-01 23:21:36 -0500

    Able to create a secret message, auto highlight the link able to cut & paste the link to share with a friend.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
-    const input = document.querySelector('input');
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
 
-    console.log(input.value);
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 });
